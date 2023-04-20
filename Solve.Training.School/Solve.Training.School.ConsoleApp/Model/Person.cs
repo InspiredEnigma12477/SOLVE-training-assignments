f488aa446c3b68fb5633c99e9e1fbaa301680027
inspiredenigma12477 <sakoreshivam3901@gmail.com>
2023-04-20 09:53:35 +0530

    school assignment

---
+++ Solve.Training.School/Solve.Training.School.ConsoleApp/Model/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solve.Training.School.ConsoleApp.Model
+{
+    public abstract class Person
+    {
+        private int age { get; set; }
+        private string name { get; set; }
+        private string gender { get; set; }
+        private string bloodGroup { get; set; }
+
+        Person(string name, int age, string gender, string bloodGroup)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.bloodGroup = bloodGroup;
+        }
+
+        public virtual void getAllInfo()
+        {
+            Console.WriteLine($"Name = {name}, Gender = {gender}, bloodGroup = {bloodGroup} ");
+        }
+    }
+}
