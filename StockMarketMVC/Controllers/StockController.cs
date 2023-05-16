98bd40ac766bcd74c4f2cfba518fb088255813b5
inspiredenigma12477 <sakoreshivam3901@gmail.com>
2023-05-16 12:09:21 +0530

    delete MVC comlete

--- StockMarketMVC/Controllers/StockController.cs
+++ StockMarketMVC/Controllers/StockController.cs
@@ -6,39 +6,54 @@ using System.Net.Http;
 
 namespace StockMarketMVC.Controllers
 {
-    public class StockController : Controller
-    {
-        private readonly ILogger<StockController> _logger;
-        private readonly StockService _stockService;
-        public StockController()
-        {
-            _stockService = new StockService();
+	[Route("[controller]")]
+	public class StockController : Controller
+	{
+		
+		private readonly ILogger<StockController> _logger;
+		private readonly StockService _stockService;
+		public StockController()
+		{
+			_stockService = new StockService();
+		}
+		[HttpGet]
+		[Route("Index")]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		[Route("AddStock")]
+		public IActionResult AddStock()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		[Route("DisplayAllStocks")]
+		public async Task<IActionResult> DisplayAllStocks()
+		{
+			ViewBag.stocks = await _stockService.GetAllStock();
+			return View();
+		}
+		
+		[HttpGet]
+		[Route("DeleteStock/{id}")]
+		public async Task<IActionResult> DeleteStock(int id)
+		{
+            await _stockService.DeleteStock(id);
+            return RedirectToAction("DisplayAllStocks", "Stock");
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult AddStock()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> DisplayAllStocks()
-        {
-            ViewBag.stocks = await _stockService.GetAllStock();
-            return View();
-        }
 
 
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
